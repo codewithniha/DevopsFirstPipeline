5d12dee928570de19fa05725c1a4398748786bb3
codewithniha <156667817+codewithniha@users.noreply.github.com>
2026-04-06 10:26:20 +0500

    Correct syntax issues in Hello class
    Fix syntax errors in Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!);
         int x = 5 / 0;
-        System.out.print("Niha ");
+        System.out.print("Niha ")
     }
 }
